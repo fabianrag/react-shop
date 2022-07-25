fd5230200c70e07d9bf329a7060ab3fe025d97d2
Fabian Araya <fbnarayaguerra@gmail.com>
2022-07-24 21:08:31 -0400

    [ADD] GitHub Actions

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import '@styles/OrderItem.scss';
+import close from '@icons/icon_close.png';
+import AppContext from '@context/AppContext';
+
+const OrderItem = ({ item }) => {
+	const { removeFromCart } = useContext(AppContext);
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={item.images[0]} alt={item.title} />
+			</figure>
+			<p>{item.title}</p>
+			<p>${item.price}</p>
+			<img src={close} alt="close" onClick={() => removeFromCart(item)} />
+		</div>
+	);
+}
+
+export default OrderItem;
